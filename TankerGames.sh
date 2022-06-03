fcc16d5342cb20a0a5b3a8c746dd5a63d22d3555
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2022-06-03 14:35:16 +0200

    Controles README

--- TankerGames.sh
+++ TankerGames.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-./Server_TankerGames.out 0.0.0.0 2200 &
+# make binaries
+make clean
+make game
 
+# open server
+./Server_TankerGames.out 0.0.0.0 2200 &
 sleep 1.5
 
+# open clients
 ./Client_TankerGames.out 127.0.0.1 2200 &
-
 ./Client_TankerGames.out 127.0.0.1 2200 &
